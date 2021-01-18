257037c84a9867bd21f78836dd068cc09139cc51
ethico <shahriar.hasan.1523@gmail.com>
2021-01-18 17:44:17 +0600

    Fixed: some css fixed & js added
    css fixed:
    -header overflow fixed
    -logo not showing in small device fixed
    -margin & padding problem fixed
    js added:
    -encryption decryption new option added
    -copy text to clipboard button added

--- js/encrypt.js
+++ js/encrypt.js
@@ -2,33 +2,43 @@
 function displayEncryption() {
     var en = document.getElementById("encryption");
     var de = document.getElementById("decryption");
+    
 
-
+                    // show hide
     en.style.display = "block";
     de.style.display = "none";
+    document.getElementById("all-part").style.display = "block";
+    document.getElementById("text-hide").style.display = "none";
 
+
+                    // button color 
     document.getElementById("btn-en").style.background = "#6C757D";
     document.getElementById("btn-de").style.background = "transparent";
 }
-// function started
-function encrypt()
-{
+// encrypt function started
+function encrypt() {
     var charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     var charSetLen = charSet.length;
+
+
     var string = document.getElementById("string-e").value;
-    var key = Math.floor(Math.random()*10%6+3);
-    var decryptedText = "";
     var strLen = string.length;
-    for(var i=0; i<strLen ; i++)
-    {
-        for(var j=0 ; j<key ; j++)
-        {
-            decryptedText+=charSet[Math.floor((Math.random()*100)%charSetLen)];
+
+
+    if (strLen != 0) {
+        var key = Math.floor(Math.random() * 10 % 6 + 3);
+        var decryptedText = "";
+
+        for (var i = 0; i < strLen; i++) {
+            for (var j = 0; j < key; j++) {
+                decryptedText += charSet[Math.floor((Math.random() * 100) % charSetLen)];
+            }
+            decryptedText += charSet[string.charCodeAt(i) % charSetLen] + Math.floor(string.charCodeAt(i) / charSetLen);
         }
-        decryptedText += charSet[string.charCodeAt(i)%charSetLen]+Math.floor(string.charCodeAt(i)/charSetLen);
+        decryptedText += key;
+        document.getElementById("output-e").value = decryptedText;
     }
-    decryptedText+=key;
-    document.getElementById("output-e").value = decryptedText;
+
 
 }
 
@@ -37,36 +47,45 @@ function displayDecryption() {
     var en = document.getElementById("encryption");
     var de = document.getElementById("decryption");
 
-
+                    // show hide
     en.style.display = "none";
     de.style.display = "block";
+    document.getElementById("all-part").style.display = "block";
+    document.getElementById("text-hide").style.display = "none";
 
-
+                    // button color 
     document.getElementById("btn-de").style.background = "#6C757D";
     document.getElementById("btn-en").style.background = "transparent";
 }
 
-// function started
-function decrypt()
-{
+
+// decrypt function started
+function decrypt() {
     var charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     var charSetLen = charSet.length;
     var string = document.getElementById("string-d").value;
     var strLen = string.length;
-    var key = Number(string[strLen-1]);
+    var key = Number(string[strLen - 1]);
     var decryptedText = "";
 
 
-    for(var i = key; i<strLen ; i=i+key)
-    {
+    for (var i = key; i < strLen; i = i + key) {
         var a = charSet.indexOf(string[i]);
-        var b = Number(string[i+1]);
+        var b = Number(string[i + 1]);
         // decryptedText += String.fromCharCode(b*charSetLen+a);
-        decryptedText += String.fromCharCode(Number(string[i+1])*charSetLen+charSet.indexOf(string[i]));
-        i=i+2;
+        decryptedText += String.fromCharCode(Number(string[i + 1]) * charSetLen + charSet.indexOf(string[i]));
+        i = i + 2;
     }
 
 
 
     document.getElementById("output-d").value = decryptedText;
+}
+
+// copy function starts
+function textCopy()
+{
+    document.getElementById("output-e").select();
+    document.getElementById("output-d").select();
+    document.execCommand("copy");
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
-#Shuttle Encryption
+# Shuttle Encryption
 
-###facilities:
-*Custom Algorithm (Self Made)
-*User friendly environment
-*Encryption & Decryption KEY is unique every click
-*Used ASCII value in algorithm
+### facilities:
+  -  Custom Algorithm (Self Made)
+  -  User friendly environment
+  -  Encryption & Decryption KEY is unique every click
+  -  Used ASCII value in algorithm
